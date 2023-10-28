47c4951d903e697bf3f55e3ce78ac44cce90312b
WSU9874 <129811877+WSU9874@users.noreply.github.com>
2023-10-28 23:27:20 +0900

    Delete Uk/10/28/a directory

--- Uk/10/28/a/b.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Main {
-    public static void main(String[] arg){
-        Scanner sc = new Scanner(System.in);
-        double a = sc.nextInt();
-        double b = sc.nextInt();
-        System.out.println(a/b);
-    }
-}

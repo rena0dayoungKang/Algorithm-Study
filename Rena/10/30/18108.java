127712f3d3dac021141349decdc26a73348740a7
rena0dayoungKang <127266915+rena0dayoungKang@users.noreply.github.com>
2023-10-30 22:04:55 +0900

    [23-10-30][18108][Rena]

---
+++ Rena/10/30/18108.java
@@ -0,0 +1,9 @@
+1998년생인 내가 태국에서는 2541년생?! - 18108
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int y = sc.nextInt();
+        System.out.println(y - 543);
+    }
+}

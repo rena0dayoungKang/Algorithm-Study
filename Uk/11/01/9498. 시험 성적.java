d5e91dcb369d915d8187cd412fc4708681592e92
WSU9874 <129811877+WSU9874@users.noreply.github.com>
2023-11-01 17:52:37 +0900

    [23-11-01] Uk - [9498. 시험 성적]
    [23-11-01] Uk - [9498. 시험 성적]

---
+++ Uk/11/01/9498. 시험 성적.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(90 <= num && num <= 100) {
+            System.out.println("A");
+        } else if(80 <= num && num <= 89) {         
+            System.out.println("B");
+        } else if(70 <= num && num <= 79) {         
+            System.out.println("C");
+        } else if(60 <= num && num <= 69) {         
+            System.out.println("D");
+        } else {         
+            System.out.println("F");
+        }
+    }
+}

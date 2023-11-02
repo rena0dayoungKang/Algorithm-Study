927fe6b4b7213c6f6a82b780983a076efa968701
WSU9874 <129811877+WSU9874@users.noreply.github.com>
2023-11-02 18:31:06 +0900

    [23-11-02] Uk - [A＋B － 3]
    [23-11-02] Uk - [A＋B － 3]

---
+++ Uk/11/02/A＋B － 3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=1;i<=num;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+            
+        }
+    }
+}
